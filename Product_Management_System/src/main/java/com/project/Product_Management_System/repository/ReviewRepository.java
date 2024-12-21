b8abea98527ec810d4346d788b793d123f295fdd
Agbame-Julius <julius.agbame@amalitech.com>
2024-12-20 20:33:39 -0800

    Added a review feature

--- Product_Management_System/src/main/java/com/project/Product_Management_System/repository/ReviewRepository.java
+++ Product_Management_System/src/main/java/com/project/Product_Management_System/repository/ReviewRepository.java
@@ -1,4 +1,12 @@
 package com.project.Product_Management_System.repository;
 
-public interface ReviewRepository {
+
+import com.project.Product_Management_System.entity.ProductReview;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface ReviewRepository extends MongoRepository<ProductReview, Integer> {
+    List<ProductReview> findByProductId(Integer productId);
 }
--- Product_Management_System/src/main/java/com/project/Product_Management_System/service/ReviewService.java
+++ Product_Management_System/src/main/java/com/project/Product_Management_System/service/ReviewService.java
@@ -1,4 +1,26 @@
 package com.project.Product_Management_System.service;
 
+import com.project.Product_Management_System.entity.ProductReview;
+import com.project.Product_Management_System.repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ReviewService {
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    public List<ProductReview> getReviewsForProduct(Integer productId) {
+        return reviewRepository.findByProductId(productId);
+    }
+
+    public ProductReview addReview(ProductReview review) {
+        return reviewRepository.save(review);
+    }
+
+    public List<ProductReview> getAllReviews() {
+        return reviewRepository.findAll();
+    }
 }
